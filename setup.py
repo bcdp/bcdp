ddc149ed5235099a7ab58295d0bd0882a7693dbb
Alex Goodman <agoodm@users.noreply.github.com>
2020-02-02 18:10:37 -0700

    Bump to v0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bcdp',
-      version='0.1.0',
+      version='0.1.1',
       description='Big Climate Data Pipeline',
       author='Alex Goodman',
       packages=['bcdp'],
